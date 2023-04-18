7e31e767ba9d05e6f77a877465b814fd015bce15
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2023-04-18 15:49:46 +0100

    Fixed import

--- src/pages/api/search-movie-titles.js
+++ src/pages/api/search-movie-titles.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from "../../../utils/database";
+import { connectToDatabase } from "@/utils/database";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
